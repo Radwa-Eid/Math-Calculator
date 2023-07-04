1b2ab3869f761760d9a6b5071c6de741287466a3
Radwa-Eid <nwarradwa@gmail.com>
2023-07-04 07:29:06 +0300

    Math Calc Js Project

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// $math.add(3,3)
+// $math.sub(3,3)
+// $math.multi(3,10)
+// $math.division(30,3)
---
+++ js/math.js
@@ -0,0 +1,16 @@
+const $math = (function () {
+    return {
+        multi: function (num1, num2) {
+            console.log(num1 * num2);
+        },
+        division: function (num1, num2) {
+            console.log(num1 / num2)
+        },
+        add: function (num1, num2) {
+            console.log(num1 + num2)
+        },
+        sub: function (num1, num2) {
+            console.log(num1 - num2)
+        }
+    }
+})();
